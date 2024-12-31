087feae2886e0dc7e31bf0752049b024c265c144
tanushreesogali <117579818+tanushreesogali@users.noreply.github.com>
2025-01-01 01:47:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
 import { Route, Routes} from "react-router-dom";
 import Home from './Home';
+import PressCorner from './Components/PressCorner';
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/tournaments" element={<div>Tournaments Page</div>} />
+        <Route path="/press-corner" element={<PressCorner/>} />
+        <Route path="/initiatives" element={<div>Initiatives Page</div>} />
+        <Route path="/about-us" element={<div>About Us Page</div>} />
+        <Route path="/login" element={<div>Login Page</div>} />
       </Routes>
     </div>
   );
 }
 
 export default App;
+
