ff6f9efaab3742955b86be2cc5055eb8e422d9be
tanushreesogali <117579818+tanushreesogali@users.noreply.github.com>
2025-01-02 16:39:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Routes} from "react-router-dom";
 import Home from './Home';
 import PressCorner from './Components/PressCorner';
 import PressCornerNews from './Components/PressCornerNews';
-import InitiativesPage from './Components/initiatives/InitiativesPage';
+// import InitiativesPage from './Components/initiatives/InitiativesPage';
 function App() {
   return (
     <div className="App">
@@ -12,7 +12,7 @@ function App() {
         <Route path="/tournaments" element={<div>Tournaments Page</div>} />
         <Route path="/press-corner" element={<PressCorner/>} />
         <Route path="/press-news" element={<PressCornerNews/>}/>
-        <Route path="/initiatives" element={<InitiativesPage/>} />
+        {/* <Route path="/initiatives" element={<InitiativesPage/>} /> */}
         <Route path="/about-us" element={<div>About Us Page</div>} />
         <Route path="/login" element={<div>Login Page</div>} />
       </Routes>
@@ -21,4 +21,5 @@ function App() {
 }
 
 export default App;
+
 
