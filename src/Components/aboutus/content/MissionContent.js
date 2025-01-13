5a253bb38cee0b3364a2073fa88fb735093b5a0a
tanushreesogali <117579818+tanushreesogali@users.noreply.github.com>
2025-01-14 00:18:01 +0530

    minor update src delete

--- src/Components/aboutus/content/MissionContent.js
+++
@@ -1,9 +0,0 @@
-const content = `
-At Football Junction, our mission is to make football accessible and enjoyable for all by providing inclusive platforms for players of every age, skill level, and background. We are committed to:
-Organizing high-quality local and corporate tournaments that foster a spirit of healthy competition and community engagement.
-Initiating grassroots development programs that offer young aspiring footballers the opportunity to train, grow, and excel under professional guidance.
-Building a community where passion for football translates into lifelong skills, discipline, and opportunities, contributing to the overall development of the sport in the region.
-Through these initiatives, we aim to create a positive, impactful presence in the football community, encouraging a love for the game while driving holistic development for every individual associated with Football Junction.
-`;
-
-export default content;
