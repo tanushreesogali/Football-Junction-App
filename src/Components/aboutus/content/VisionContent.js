c2a5fbdd7dd5146b78ed2b55a3c1ac604513a849
tanushreesogali <117579818+tanushreesogali@users.noreply.github.com>
2025-01-15 00:49:18 +0530

    final1 changes src delete

--- src/Components/aboutus/content/VisionContent.js
+++
@@ -1,7 +0,0 @@
-const content = `
-At Football Junction, we envision a dynamic and inclusive football ecosystem that becomes a cornerstone of Kolkata’s sporting culture. Our goal is to transform the city into a hub for football development, where talent is identified and nurtured at every stage, from aspiring young players to seasoned professionals.
-
-We strive to create an environment where football is more than just a game—it’s a way to bring communities together, promote healthy lifestyles, and foster personal growth. By offering a range of programs, tournaments, and grassroots initiatives, we aim to provide opportunities for everyone to experience the sport, regardless of age, background, or skill level.
-`;
-
-export default content;
